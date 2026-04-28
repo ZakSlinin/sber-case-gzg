4d182629ad65967076cd524b93cbd5b3902eb0ec
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-04-28 09:05:21 +0300

    delete table

--- email-verification/db/migrations/000001_create_tokens_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tokens (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    email VARCHAR(255) NOT NULL,
-    token UUID NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    expires_at TIMESTAMP NOT NULL
-);
